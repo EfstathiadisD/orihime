d21ff98709c0a5b1ac65bcfb7c5b78d405cb3d97
EfstathiadisD <EfstathiadisD@protonmail.com>
2022-05-14 02:34:37 +0300

    feat(build): ORI-6675/ More improvements fot commit management

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,13 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  rules: {
+    "type-case": () => Promise.resolve([2, "always", "lower-case"]),
+    "type-enum": () =>
+      Promise.resolve([2, "always", ["feat", "fix", "refactor", "revert", "chore", "build"]]),
+    "subject-case": () => Promise.resolve([2, "always", ["sentence-case"]]),
+    "scope-case": () => Promise.resolve([2, "always", "lower-case"]),
+  },
+  extends: ["@commitlint/config-conventional", "@commitlint/config-nx-scopes"],
+  parserPreset: {
+    headerCorrespondence: ["type", "scope", "ticket", "subject"],
+  },
+};
